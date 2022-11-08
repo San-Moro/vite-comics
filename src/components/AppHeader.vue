697c439522b350e426e22b64f15b851bbbffb867
San-Moro <valentinacortese.dsg@gmail.com>
2022-11-08 20:02:45 +0100

    Header done

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,13 +1,137 @@
 <script>
 export default {
-    name: "AppHeader"
+    name: "AppHeader",
+    data () {
+        return {
+            linksNav: [
+                {
+                    title: "CHARACTERS",
+                    href: "",
+                    active: false,
+                },
+                {
+                    title: "COMICS",
+                    href: "",
+                    active: true,
+                },
+                {
+                    title: "MOVIE",
+                    href: "",
+                    active: false,
+                },
+                {
+                    title: "TV",
+                    href: "",
+                    active: false,
+                },
+                {
+                    title: "GAMES",
+                    href: "",
+                    active: false,
+                },
+                {
+                    title: "COLLECTIBLES",
+                    href: "",
+                    active: false,
+                },
+                {
+                    title: "VIDEOS",
+                    href: "",
+                    active: false,
+                },
+                {
+                    title: "FANS",
+                    href: "",
+                    active: false,
+                },
+                {
+                    title: "NEWS",
+                    href: "",
+                    active: false,
+                },
+                {
+                    title: "SHOP",
+                    href: "",
+                    active: false,
+                }
+
+            ]
+        }
+    }
 }
 </script>
 
 <template>
-    <div>
-        <h1>Header</h1>
-    </div>
+    <header>
+        <div class="container">
+            <div class="logo-header">
+                <img src="../assets/img/dc-logo.png" alt="">
+            </div>
+            <nav class="nav-header">
+                <ul>
+                    <li v-for="(link, index) in linksNav" :key="index">
+                        <a :class="{'active' : link.active}" href="">{{link.title}}</a>
+                    </li>
+                    
+                </ul>
+            </nav>
+        </div>
+    </header>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@use "../styles/partials/mixins" as *;
+@use "../styles/partials/variables" as *;
+
+header {
+    width: 100%;
+    height: 120px;
+    background-color: white;
+    // DEBUG
+    border: 1px solid black;
+
+    .container {
+        width: 70%;
+        height: 100%;
+        margin: 0 auto;
+        @include flex-center("vertical");
+        position: relative;
+        
+        .logo-header {
+            img {
+                width: 80px;
+                height: 80px;
+            }
+        }
+
+        .nav-header {
+            
+            position: absolute;
+            right: 0;
+
+            ul {
+                height: 120px;
+                list-style-type: none;
+                @include flex-center("vertical");
+
+                li {
+                    margin: 0 15px;
+                    
+                    a {
+                        color: $secondary-color;
+                        font-size: 13px;
+                        font-weight: 900;
+                        &.active {
+                            color: $primary-color;
+                            border-bottom: 2px solid $primary-color;
+                        }
+
+                    }
+                }
+            }
+        }
+    
+
+    }
+}
+</style>
